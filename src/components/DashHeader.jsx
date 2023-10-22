2d1fbc334240962d22a07b10fb542bf233e00f3b
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-10-22 21:00:22 +0200

    make logout

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,13 +1,57 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+
+import { useSendLogoutMutation } from "../features/auth/authApiSlice";
+
+const DASH_REGEX = /^\/dash(\/)?$/;
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
+const USERS_REGEX = /^\/dash\/users(\/)?$/;
 
 const DashHeader = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/");
+    }
+  }, [isSuccess, navigate]);
+
+  const onLogoutClicked = () => sendLogout();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message} </p>;
+  }
+
+  let dashClass = null;
+  if (
+    !DASH_REGEX.test(pathname) &&
+    !NOTES_REGEX.test(pathname) &&
+    !USERS_REGEX.test(pathname)
+  ) {
+    dashClass = "dash-header__container--small";
+  }
+
   return (
     <header className="dash-header">
-      <div className="dash-header__container">
+      <div className={`dash-header__container ${dashClass}`}>
         <Link to="/dash">
           <h1 className="dash-header__title">techNotes</h1>
         </Link>
-        <nav className="dash-header__nav">{/* add nav buttons later */}</nav>
+        <nav className="dash-header__nav">
+          <button className="icon-button" title="Logout" onClick={sendLogout}>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+          </button>
+        </nav>
       </div>
     </header>
   );
