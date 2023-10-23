84698ba1263e1273b08cb87049d405cc44c797cd
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-10-23 12:21:41 +0200

    Role based login
    Split which functions and links can make admins, managers and emloyees

--- src/components/DashFooter.jsx
+++ src/components/DashFooter.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const DashFooter = () => {
   const navigate = useNavigate();
@@ -23,11 +24,13 @@ const DashFooter = () => {
     );
   }
 
+  const { username, status } = useAuth();
+
   return (
     <footer className="dash-footer">
       {goHomeButton}
-      <p>Current User:</p>
-      <p>Status:</p>
+      <p>Current User: {username}</p>
+      <p>Status: {status}</p>
     </footer>
   );
 };
--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Welcome = () => {
   const date = new Date();
@@ -8,22 +9,28 @@ const Welcome = () => {
     timeStyle: "long",
   }).format(date);
 
+  const { username, isManager, isAdmin } = useAuth();
+
   return (
     <section className="welcome">
       <p>{today}</p>
-      <h1>Welcome!</h1>
+      <h1>Welcome {username}!</h1>
       <p>
         <Link to="/dash/notes">View techNotes</Link>
       </p>
       <p>
         <Link to="/dash/notes/new">Add New Note</Link>
       </p>
-      <p>
-        <Link to="/dash/users">View User Settings</Link>
-      </p>
-      <p>
-        <Link to="/dash/users/new">Add New User</Link>
-      </p>
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to="/dash/users">View User Settings</Link>
+        </p>
+      )}
+      {(isManager || isAdmin) && (
+        <p>
+          <Link to="/dash/users/new">Add New User</Link>
+        </p>
+      )}
     </section>
   );
 };
