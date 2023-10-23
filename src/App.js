df582df2d9933c2e913d0879e80677c23f04bc98
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-10-23 13:16:38 +0200

    Public/private routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import EditNote from "./features/notes/EditNote"
 import NewNote from "./features/notes/NewNote"
 import Prefetch from "./features/auth/Prefetch"
 import PersistLogin from "./features/auth/PersistLogin"
+import RequireAuth from "./features/auth/RequireAuth";
+import {ROLES} from './config/roles'
 
 function App() {
   return (
@@ -21,15 +23,18 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
           <Route element={<Prefetch />}>
           <Route path="dash" element={<DashLayout />} >
 
           <Route index element={<Welcome />} />
 
-          <Route path="users">
-            <Route index element={<UsersList />} />
-            <Route path=":id" element={<EditUser />} />
-            <Route path="new" element={<NewUserForm />} />
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Manager]} />}>
+            <Route path="users">
+              <Route index element={<UsersList />} />
+              <Route path=":id" element={<EditUser />} />
+              <Route path="new" element={<NewUserForm />} />
+            </Route>
           </Route>
 
           <Route path="notes" >
@@ -39,6 +44,7 @@ function App() {
           </Route>
 
           </Route>
+        </Route>
         </Route>
         </Route>
 
